e40a47f12d93683212cba30814e867e96e67f8f1
lidaibin <lidaibin@cmcm.com>
2018-10-26 15:53:21 +0800

    init: fix path

--- config/index.js
+++ config/index.js
@@ -9,7 +9,7 @@ module.exports = {
 
     // Paths
     assetsSubDirectory: 'static',
-    assetsPublicPath: '/',
+    assetsPublicPath: './',
     // proxyTable: {
     //   '/api': {
     //     target: 'http://139.159.210.220',  //目标接口域名
@@ -67,7 +67,7 @@ module.exports = {
     // Paths
     assetsRoot: path.resolve(__dirname, '../dist'),
     assetsSubDirectory: 'static',
-    assetsPublicPath: '/',
+    assetsPublicPath: './',
 
     /**
      * Source Maps
