ca2100d21c87ebdb0084665ecb4f1f96233a993a
lidaibin <lidaibin@cmcm.com>
2018-10-26 17:43:42 +0800

    feature: ws port change

--- src/config/index.js
+++ src/config/index.js
@@ -13,13 +13,13 @@ if (isMicromessenger && isMicromessenger.length > 0) {
 if (process.env.NODE_ENV === 'development') {
     isTest = true;
     console.log('内网测试环境')
-    url = "ws://10.12.130.110:3000"; // 内网网关
+    url = "ws://10.12.130.110:3843"; // 内网网关
     // httpUrl = 'http://10.0.3.41'; // 内网http服务ip
 
 } else {
     console.log('外网测试环境');
     isTest = false;
-    url = "ws://139.159.210.220:3000"; // 外网网关
+    url = "ws://139.159.210.220:3843"; // 外网网关
     // httpUrl = 'http://120.79.88.184'; // 外网http服务
 }
 export default {
