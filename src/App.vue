e45c4f58ce3db03eee8fac3e9c791e99856baa96
lidaibin <lidaibin@cmcm.com>
2018-11-08 15:48:43 +0800

    feature: switch to dev platform

--- src/App.vue
+++ src/App.vue
@@ -57,12 +57,12 @@
           deviceid: this.id,
         })
           .then((res) => {
-            console.log("获取签名", res);
+            console.log("获取签名", res, 'wxaca505e7b260c383');
             if (res.data) {
               wx.config({
                 debug: false, // 开启调试模式,调用的所有api的返回值会在客户端alert出来，若要查看传入的参数，可以在pc端打开，参数信息会通过log打出，仅在pc端时才会打印。
-                // appId: 'wxaca505e7b260c383', // 必填，公众号的唯一标识
-                appId: 'wx847ad179e85ad077', // 测试公众号的唯一标识
+                appId: 'wxaca505e7b260c383', // 必填，公众号的唯一标识
+                // appId: 'wx847ad179e85ad077', // 测试公众号的唯一标识
                 timestamp, // 必填，生成签名的时间戳
                 nonceStr, // 必填，生成签名的随机串
                 signature: res.data, // 必填，签名
