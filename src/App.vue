952c445f91dbf578c7d8656f9ea2f47dc01c6030
lidaibin <lidaibin@cmcm.com>
2018-11-08 14:16:43 +0800

    feature: switch to test platform

--- src/App.vue
+++ src/App.vue
@@ -61,7 +61,8 @@
             if (res.data) {
               wx.config({
                 debug: false, // 开启调试模式,调用的所有api的返回值会在客户端alert出来，若要查看传入的参数，可以在pc端打开，参数信息会通过log打出，仅在pc端时才会打印。
-                appId: 'wxaca505e7b260c383', // 必填，公众号的唯一标识
+                // appId: 'wxaca505e7b260c383', // 必填，公众号的唯一标识
+                appId: 'wx847ad179e85ad077', // 测试公众号的唯一标识
                 timestamp, // 必填，生成签名的时间戳
                 nonceStr, // 必填，生成签名的随机串
                 signature: res.data, // 必填，签名
@@ -78,6 +79,22 @@
          * */
         wx.ready(function () {
           console.log("微信验证成功");
+          wx.updateAppMessageShareData({
+            title: '测试分享好友01', // 分享标题
+            desc: 'emmmmm', // 分享描述
+            link: `http://www.wxapidev.cn/wxsimulator/?deviceid=${this.id}&share=1`, // 分享链接，该链接域名或路径必须与当前页面对应的公众号JS安全域名一致
+            imgUrl: '', // 分享图标
+          }, function(res) {
+              //这里是回调函数
+          });
+          wx.updateTimelineShareData({
+            title: '测试分享朋友圈01', // 分享标题
+            link: `http://www.wxapidev.cn/wxsimulator/?deviceid=${this.id}&share=1`, // 分享链接，该链接域名或路径必须与当前页面对应的公众号JS安全域名一致
+            imgUrl: '', // 分享图标
+          }, function(res) {
+              //这里是回调函数
+          });
+
         })
         const self = this;
         wx.error(function(res){
